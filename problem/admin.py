f9c7ec863fc464719a64e6ada03cef2c16caa128
ultmaster <scottyugochang@hotmail.com>
2016-12-21 19:13:24 +0800

    Add models of 'problem' and 'data'

---
+++ problem/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Problem
+from .models import Data
+
+admin.site.register(Problem)
+admin.site.register(Data)
---
+++ problem/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Data(models.Model):
+    STATUS_CHOICES = (
+        ('s', 'Sample'),
+        ('d', 'Test'),
+        ('r', "Removed"),
+    )
+
+    input = models.TextField('Input')
+    output = models.TextField('Output')
+    explanation = models.TextField('Explanation')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Problem(models.Model):
+    STATUS_CHOICES = (
+        ('r', 'Removed'),
+        ('a', 'Available'),
+    )
+
+    id = models.IntegerField('#', primary_key=True)
+    title = models.CharField('Title', max_length=70)
+    submissions = models.IntegerField('Submissions', default=0)
+    status = models.CharField('Status', max_length=1, choices=STATUS_CHOICES)
+    likes = models.PositiveIntegerField('Likes', default=0)
+
+    description = models.TextField('Description')
+    input = models.TextField('Input')
+    output = models.TextField('Output')
+    samples = models.ManyToManyField(Data, 'Samples')
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.title
+
+    class Meta:
+        ordering = ['-id']
+
+# TODO: There is a redirect issue when saving Data through Problem
