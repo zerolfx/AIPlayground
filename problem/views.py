426c504df7193ac10745df6c34d591e331170b10
zerol <zerolfx0@gmail.com>
2016-12-23 20:54:36 +0800

    add: support md

--- problem/views.py
+++ problem/views.py
@@ -10,8 +10,8 @@ def problist_view(request):
 
 def problem_view(request, get_id):
     problem = Problem.objects.get(id=get_id)
-    # problem.description = markdown2.markdown(problem.description, )
-    # problem.input = markdown2.markdown(problem.input)
-    # problem.output = markdown2.markdown(problem.output)
+    problem.description = markdown2.markdown(problem.description, )
+    problem.input = markdown2.markdown(problem.input)
+    problem.output = markdown2.markdown(problem.output)
     return render(request, 'problem.html', {'problem': problem})
 
