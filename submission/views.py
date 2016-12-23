308d7bc7b6df896152f2347c79edd28817bcb64a
zerol <zerolfx0@gmail.com>
2016-12-23 21:36:33 +0800

    add: requirement celery

--- submission/views.py
+++ submission/views.py
@@ -3,6 +3,7 @@ from .models import Submission
 from .models import Run
 from problem.models import Problem
 
+
 def submission_view(request, get_id):
     submission = Submission.objects.get(id=get_id)
     problem = Problem.objects.get(id=submission.problem_id)
