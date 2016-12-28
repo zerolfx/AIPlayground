d1bbab20a48040862c82d8d659047d1d95356d25
zerol <zerolfx0@gmail.com>
2016-12-28 20:50:06 +0800

    fix: (small) test relative path

--- test/test.py
+++ test/test.py
@@ -1,7 +1,8 @@
 import sys; print('Python %s on %s' % (sys.version, sys.platform))
 import django; print('Django %s' % django.get_version())
 import os
-sys.path.extend(['/home/zerol/PycharmProjects/AIPlayground', '/home/zerol/Software/pycharm/helpers/pycharm', '/home/zerol/Software/pycharm/helpers/pydev'])
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.extend([BASE_DIR])
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AIPlayground.settings")
 django.setup()
 # import django_manage_shell; django_manage_shell.run("/home/zerol/PycharmProjects/AIPlayground")
@@ -9,10 +10,8 @@ django.setup()
 from submission.models import *
 from core.runner import controller
 import shutil
-import os
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 rd = Round.objects.all().first()
 path = os.path.join(BASE_DIR, 'workspace', 'run', str(rd.id))
 print(path)
