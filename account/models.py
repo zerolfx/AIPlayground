6993adc53fa7b516900f9080a0eda2a43f362e9d
ultmaster <scottyugochang@hotmail.com>
2016-12-29 19:34:13 +0800

    Message wrong...

--- account/models.py
+++ account/models.py
@@ -4,7 +4,14 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    email = models.CharField('email', max_length=30, blank=True)
+    email = models.CharField('Email', max_length=80, blank=True)
+    first_name = models.CharField('First Name', max_length=30, blank=True)
+    last_name = models.CharField('Last Name', max_length=30, blank=True)
+    birth_date = models.DateField('Birth Date', blank=True)
+    country = models.CharField('Country', max_length=30, blank=True)
+    city = models.CharField('City', max_length=30, blank=True)
+    organization = models.CharField('Organization', max_length=80, blank=True)
+
     rating = models.IntegerField('rating', default=0)
 
     def __str__(self):
