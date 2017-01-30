c6b1bf9feadfec62268e51e03e60123fd488f490
ultmaster <scottyugochang@hotmail.com>
2017-01-31 01:32:00 +0800

    getting started, finally

--- static/js/init.js
+++ static/js/init.js
@@ -2,8 +2,15 @@ var vm = new Vue({
   el: '#example',
   data: {
     classObject: {
+      "light-blue": true,
+      "lighten-1": true
+    },
+    classObject2: {
       "nav-wrapper": true,
       "container": true
+    },
+    classObject3: {
+      "light-blue": true
     }
   }
 });
